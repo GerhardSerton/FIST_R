84ada40cd2f2522cab8bb651f07c9cc7b37ebe97
Gerhard Serton <gerhardserton@gmail.com>
2024-01-17 22:21:02 +0200

    Added basic layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,13 +9,25 @@ import Button from '@mui/material/Button';
 
 import KIAButton from '../../components/KIAButton/KIAButton';
 import { Grid } from '@mui/material';
+import SheetSections from '../../components/SheetSections/SheetSections';
 
 export function Home() {
 	return (
 		<div class="home">
 			<Grid container>
 				<Grid item xs={12}>
-					<KIAButton onClick={() => {console.log("Clicked!")}}/>
+						<KIAButton onClick={() => {console.log("Clicked!")}}/>
+				</Grid>
+			</Grid>
+			<Grid container direction={'column'} className="container">
+				<Grid item xs={6} className="sector">
+					<SheetSections title='' long={false}/>
+				</Grid>
+				<Grid item xs={6} className="sector">
+					<SheetSections title='Inventory' long={false}/>
+				</Grid>
+				<Grid item xs={6} className="sector traits">
+					<SheetSections title='Traits' long={true}/>
 				</Grid>
 			</Grid>
 		</div>
