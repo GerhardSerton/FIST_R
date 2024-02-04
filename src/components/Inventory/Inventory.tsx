f4b8f1e76529ec3dc0fa04726c2c4ae29bf29617
Gerhard Serton <gerhardserton@gmail.com>
2024-02-04 13:54:23 +0200

    Fixed issues with the Inventory and Traits blocks not populating.

--- src/components/Inventory/Inventory.tsx
+++ src/components/Inventory/Inventory.tsx
@@ -1,15 +1,18 @@
 import { List, ListItem } from "@mui/material";
 import styles from "./Inventory.module.css";
+import { Item } from "../../types/character";
 
 interface InventoryProps {
-  items: Array<string> | undefined;
+  items: Array<Item> | undefined;
 }
 
 export default function Inventory(props: InventoryProps) {
   const itemList = props.items.map((x) => {
     return (
       <>
-        <ListItem className={styles.listItem}>&#x2022; {x}</ListItem>
+        <ListItem key={x.Text} className={styles.listItem}>
+          &#x2022; {x.Text}
+        </ListItem>
       </>
     );
   });
