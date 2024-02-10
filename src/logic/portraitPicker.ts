8a550d831fd6703cb54b8898ca9d71408a787019
Elle Fieggen <laf890@gmail.com>
2024-02-10 12:54:52 +0200

    Fix Build with Asset wonk

--- src/logic/portraitPicker.ts
+++ src/logic/portraitPicker.ts
@@ -2,5 +2,5 @@ import { d6 } from "./diceRoller";
 
 export default function portraitPicker() {
   const rollDice = d6() - 1;
-  return `/src/assets/portraits/agent_${rollDice}.png`;
+  return `assets/portraits/agent_${rollDice}.png`;
 }
