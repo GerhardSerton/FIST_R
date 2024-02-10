992472ba4cf07a9befce9fa84a4a36d8aa1fa65d
Elle Fieggen <laf890@gmail.com>
2024-02-10 16:00:07 +0200

    Fix Fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,4 @@
 import "./style.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { useEffect, useMemo, useState } from "react";
 
 import KIAButton from "../../components/KIAButton/KIAButton";
